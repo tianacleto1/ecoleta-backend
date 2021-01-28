ba38ffe05e2404ff5c1e2f5173c8fe4c45eb02ef
Tiago Silva <tiago.silva6@br.bosch.com>
2021-01-28 13:29:12 -0300

    Implemented get items route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import Knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, resp) => {
-    return resp.json({ message: 'hello world!' })
+routes.get('/items', async (req, resp) => {
+    const items = await Knex('items').select('*');
+
+    const serializedItems = items.map(items => {
+        return { 
+            name: items.title,
+            image_url: `http://localhost:3333/uploads/${items.image}`,
+         };
+    });
+
+    return resp.json(serializedItems);
 });
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
 
-app.listen(3333);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-// stopped at 00:58 - video 2
+app.listen(3333);
