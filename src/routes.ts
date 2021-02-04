def94b0223a74237062344c8fd0c9b219f10c624
Tiago Silva <tiago.silva6@br.bosch.com>
2021-02-04 15:56:08 -0300

    Added endpoint to create new spots

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import Knex from './database/connection';
 
 const routes = express.Router();
@@ -8,12 +8,53 @@ routes.get('/items', async (req, resp) => {
 
     const serializedItems = items.map(items => {
         return { 
-            name: items.title,
+            id: items.id,
+            title: items.title,
             image_url: `http://localhost:3333/uploads/${items.image}`,
          };
     });
 
     return resp.json(serializedItems);
+});
+
+routes.post('/spots', async (req, resp) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude, 
+        city,
+        state,
+        items
+    } = req.body;
+
+    const trx = await Knex.transaction();
+
+     
+    const insertedIds = await trx('spots').insert({
+        image: 'fake-image',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude, 
+        city,
+        state
+    });
+
+    const spot_id = insertedIds[0];
+
+    const spotItems = items.map((item_id: Number) => {
+        return {
+            item_id,
+            spot_id,
+        };
+    });
+
+    await trx('spots_items').insert(spotItems);
+
+    return resp.json({ success: true });
 });
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,5 @@ app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
+
+// stopped at 01:23 - video 2
